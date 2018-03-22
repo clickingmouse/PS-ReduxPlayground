92f5d711253e5dd21adc5a33a1ba66d385c76794
clickingmouse <clickingmouse@users.noreply.github.com>
2018-03-22 10:13:57 +0800

    redux basic complete

---
+++ src/actions/courseAction.js
@@ -0,0 +1,11 @@
+import * as types from './actionTypes'
+
+export function createCourse(course){
+//  debugger;
+  return {type: types.CREATE_COURSE, course: course};
+  // or just "course"
+
+
+
+
+}
